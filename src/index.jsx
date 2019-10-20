10ff6e82139fff7f94eec24cd042f753e10f3df8
Victor Jimenez <vic.jicama@gmail.com>
2019-10-20 18:19:49 +0200

    Add actions to peformers in plugin server

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import * as Version from "./version";
 import * as Dependencies from "./dependencies";
 import * as Build from "./build";
+import * as Utils from "./utils";
 
-export { Version, Dependencies, Build };
+export { Version, Dependencies, Build, Utils };
